6ffa5fce97136050d9da95ede79d54961fbd8cb4
Torin Foss <tfoss@csumb.edu>
2019-01-19 02:54:31 -0800

    Functionalized

--- DetectingFace.py
+++ DetectingFace.py
@@ -9,13 +9,18 @@ PERSON_GROUP_ID = "people_two"
 CF.BaseUrl.set(BASE_URL)
 CF.Key.set(SUB_KEY)
 
-response = CF.face.detect('tejas-test.jpg')
-face_ids = [d['faceId'] for d in response]
-print(face_ids)
+def return_message_from_face(path_to_img):
 
-identified_faces = CF.face.identify(face_ids, PERSON_GROUP_ID)
-print(identified_faces)
 
-person_id = identified_faces[0]['candidates'][0]['personId']
-response = CF.person.get(PERSON_GROUP_ID, person_id)
-print(response['name'])
+    response = CF.face.detect(path_to_img)
+    face_ids = [d['faceId'] for d in response]
+    #print(face_ids)
+
+    identified_faces = CF.face.identify(face_ids, PERSON_GROUP_ID)
+    #print(identified_faces)
+
+    person_id = identified_faces[0]['candidates'][0]['personId']
+    response = CF.person.get(PERSON_GROUP_ID, person_id)
+    return(response)
+
+#print(return_message_from_face("tejas-test.jpg"))
