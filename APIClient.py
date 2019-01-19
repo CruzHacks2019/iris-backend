c7c64d2b62977428f136cc64302c90e3233a1856
Torin Foss <tfoss@csumb.edu>
2019-01-19 10:55:50 -0800

    Refactored, Created APIClient

---
+++ APIClient.py
@@ -0,0 +1,43 @@
+import cognitive_face as CF
+from APIKey import *
+import glob
+"""
+2019 Cruzhacks
+"""
+class APIClient:
+
+    def __init__(self, db_id):
+        self.BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'
+        self.PERSON_GROUP_ID = db_id
+        CF.BaseUrl.set(self.BASE_URL)
+        CF.Key.set(SUB_KEY)
+
+    def create_database(self, name):
+        CF.person_group.create(self.PERSON_GROUP_ID, name)
+
+    def add_person(self, name, user_data, img_dir):
+        response = CF.person.create(self.PERSON_GROUP_ID, name, user_data)
+        person_id = response["personId"]
+        for img in glob.glob(img_dir):
+            CF.person.add_face(img, self.PERSON_GROUP_ID, person_id)
+
+    def return_message_from_face(self, path_to_img):
+        response = CF.face.detect(path_to_img)
+        face_ids = [d['faceId'] for d in response]
+        identified_faces = CF.face.identify(face_ids, self.PERSON_GROUP_ID)
+        person_id = identified_faces[0]['candidates'][0]['personId']
+        response = CF.person.get(self.PERSON_GROUP_ID, person_id)
+        return response 
+
+    def print_status(self):
+        response = CF.person_group.get_status(self.PERSON_GROUP_ID)
+        status = response['status']
+        print(status)   
+
+    def print_list(self):
+        print(CF.person.lists(self.PERSON_GROUP_ID))
+
+    def train_data(self):
+        CF.person_group.train(self.PERSON_GROUP_ID)
+
+
--- App.py
+++ App.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, jsonify, request
 from flask_bootstrap import Bootstrap
 import base64
 from hashlib import md5
-from DetectingFace import *
+from APIClient import *
 """
 
 """
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+client = APIClient("people_three")
 
 @app.route("/")
 def index():
@@ -16,20 +16,15 @@ def index():
 
 @app.route('/detect-face', methods=['POST'])
 def detect_face():
-    # TODO:
-    #kill_me = {"Name":"Cruz Hacks" , "Project":"ALzheimers"}
-    
     img_content = request.data
     decoded_img = base64.b64decode(img_content)
-    #API CALL RIGHT HERE
-    #print(decoded_img)
-    # Temporarily save image as file
     img_path = "uploads/" + md5(img_content.decode().encode('utf-8')).hexdigest() + ".png"
     with open(img_path, "wb") as fh:
         fh.write(decoded_img)
-    result = return_message_from_face(img_path)
+
+    result = client.return_message_from_face(img_path)
     result['msg'] = "You met " + result["name"] + " he is your " + result["userData"] + "."
-    return jsonify(result) # will have to returned json dat from API call
+    return jsonify(result)
 
 if __name__=='__main__':
     app.run(debug=True)
