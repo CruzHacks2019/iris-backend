fe1b323cf6e22f8eec283abf92c21d15643a3249
Tejas Shah <tejashah88@gmail.com>
2019-01-20 02:31:20 -0800

    cleanup pt 1

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ root = db.reference()
 # Imports the Google Cloud client library
 from google.cloud import storage
 
-# Instantiates a client
-storage_client = storage.Client()
-
 # The name for the new bucket
 bucket_name = 'history-images-3519435695'
 # bucket = storage_client.create_bucket(bucket_name)
