a0124caa6847a1d26d30322a346b4007303c1434
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-01-19 16:41:31 -0800

    start working on history

--- App.py
+++ App.py
@@ -3,6 +3,15 @@ from flask_bootstrap import Bootstrap
 import base64
 from hashlib import md5
 from APIClient import *
+
+# Imports the Google Cloud client library
+from google.cloud import storage
+
+# Instantiates a client
+storage_client = storage.Client()
+
+# The name for the new bucket
+bucket_name = 'training-images-3519435695'
 """
 
 """
@@ -22,9 +31,19 @@ def detect_face():
     with open(img_path, "wb") as fh:
         fh.write(decoded_img)
 
-    result = client.return_message_from_face(img_path)
-    result['msg'] = "You met " + result["name"] + " he is your " + result["userData"] + "."
-    return jsonify(result)
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    destination_blob_name = md5(img_content.decode().encode('utf-8')).hexdigest()
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename("uploads/" + destination_blob_name + ".png")
+    print('File {} uploaded to {}.'.format("uploads/" + destination_blob_name + ".png", destination_blob_name))
+
+    url = "https://storage.cloud.google.com/training-images-3519435695/"+ destination_blob_name
+
+    # result = client.return_message_from_face(img_path)
+    # result['msg'] = "You met " + result["name"] + " he is your " + result["userData"] + "."
+    # return jsonify(result)
 
 if __name__=='__main__':
     app.run(debug=True)
